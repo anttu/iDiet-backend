1025a04fc3c28c3c9be8d8c4160145ad94fc6982
Antero Fagerstedt <antero.fagerstedt@iki.fi>
2016-12-26 16:57:31 +0200

    API for getting the authorization link for iDiet

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,14 +1,33 @@
-var express = require('express');
-var app = express();
+import OAuth from 'oauth';
 
-var API_HOST = 'https://oauth.withings.com';
+const express = require('express');
 
+const app = express();
 
-app.get('/', function(req, res) {
-    var API_KEY = process.env.API_KEY;
-    var API_SECRET = process.env.API_SECRET;
+/**
+Get the URL from which an end user can authorize iDiet to access his/her data
+*/
+app.get('/authorizelink', (req, res) => {
+    const API_KEY = process.env.API_KEY;
+    const API_SECRET = process.env.API_SECRET;
 
-    res.send(JSON.stringify([API_KEY, API_SECRET]));
+    const oauth = new OAuth.OAuth(
+     'https://oauth.withings.com/account/request_token',
+     'https://oauth.withings.com/account/access_token',
+     API_KEY,
+     API_SECRET,
+     '1.0',
+     'http://localhost:3000/',
+     'HMAC-SHA1',
+   );
+
+    oauth.getOAuthRequestToken((err, token, tokenSecret, parsedQueryString) => {
+        res.send(JSON.stringify({
+            token: token,
+            tokenSecret: tokenSecret,
+            url: oauth.signUrl('https://oauth.withings.com/account/authorize', token, tokenSecret),
+        }));
+    });
 });
 
 app.listen(3000);
