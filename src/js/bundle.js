ff3f3b48f6c0b27f54ca71485ffa82a6c0e67ac2
Antero Fagerstedt <antero.fagerstedt@iki.fi>
2016-12-26 17:01:29 +0200

    remove unused data from response

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -12,19 +12,17 @@ app.get('/authorizelink', (req, res) => {
     const API_SECRET = process.env.API_SECRET;
 
     const oauth = new OAuth.OAuth(
-     'https://oauth.withings.com/account/request_token',
-     'https://oauth.withings.com/account/access_token',
-     API_KEY,
-     API_SECRET,
-     '1.0',
-     'http://localhost:3000/',
-     'HMAC-SHA1',
+        'https://oauth.withings.com/account/request_token',
+        'https://oauth.withings.com/account/access_token',
+        API_KEY,
+        API_SECRET,
+        '1.0',
+        'http://localhost:3000/',
+        'HMAC-SHA1',
    );
 
-    oauth.getOAuthRequestToken((err, token, tokenSecret, parsedQueryString) => {
+    oauth.getOAuthRequestToken((err, token, tokenSecret) => {
         res.send(JSON.stringify({
-            token: token,
-            tokenSecret: tokenSecret,
             url: oauth.signUrl('https://oauth.withings.com/account/authorize', token, tokenSecret),
         }));
     });
